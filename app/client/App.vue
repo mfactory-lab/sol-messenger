05d53668485b6c38c06045f5a0b98954603e0451
tiamo <vk.tiamo@gmail.com>
2022-05-24 15:21:39 +0300

    feat: client app for signature testing

---
+++ app/client/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    hello world
+    <wallet-multi-button/>
+
+    <br>
+
+    <button class="swv-button sign-button" @click="handleSign">Test sign</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import {
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+
+import { initWallet, useWallet, WalletMultiButton } from 'solana-wallets-vue';
+
+import 'solana-wallets-vue/styles.css';
+import { base58ToBytes, bytesToBase58 } from "../crypto/utils";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import nacl from "tweetnacl";
+import { generateCEK } from "../crypto";
+
+const walletOptions = {
+  wallets: [
+    new PhantomWalletAdapter(),
+    new SlopeWalletAdapter(),
+    new SolflareWalletAdapter(),
+  ],
+  autoConnect: true,
+}
+initWallet(walletOptions);
+
+
+const wallet = useWallet();
+
+export default defineComponent({
+  components: {
+    WalletMultiButton
+  },
+  methods: {
+    async handleSign() {
+      // stored in account
+      const nonce = 'secret123';
+      const nonceUint8 = new TextEncoder().encode(nonce);
+      const signature = await wallet.signMessage.value(nonceUint8);
+
+      console.log('signature:', bytesToBase58(signature.slice(0, 32)))
+      console.log('signature:', bytesToBase58(signature.slice(32)))
+
+      const key = Keypair.fromSeed(signature.slice(32));
+
+      console.log(key.secretKey)
+
+      const pubKeyUint8 = wallet?.publicKey.value.toBytes();
+
+      const isVerified = nacl.sign.detached.verify(nonceUint8, signature, pubKeyUint8)
+
+      console.log(isVerified);
+
+      // ...
+    }
+  }
+})
+</script>
+
+<style>
+.sign-button {
+  background-color: darkcyan;
+}
+</style>
