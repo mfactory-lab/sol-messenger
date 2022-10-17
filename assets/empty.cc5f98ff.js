47477ec3409999f7659d79c2a9c80db61a3939b6
tiamo <vk.tiamo@gmail.com>
2022-10-17 16:39:06 +0000

    Deploying to gh-pages from @ mfactory-lab/sol-messenger@d3118364eff145b6095ce5ac1c160ff721c97e9f 🚀

---
+++ assets/empty.cc5f98ff.js
@@ -0,0 +1,1 @@
+import{g as o,k as r,o as s}from"./vendor.c5367e08.js";import{_ as t}from"./index.250fc19d.js";const n={};function c(_,a){const e=o("router-view");return s(),r(e)}const u=t(n,[["render",c],["__file","/home/runner/work/sol-messenger/sol-messenger/packages/ui/src/layouts/empty.vue"]]);export{u as default};
