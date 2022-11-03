a6388b6b38c054aba45a1d1af15c672b4b00cca3
tiamo <vk.tiamo@gmail.com>
2022-11-03 19:49:33 +0200

    feat: added `cli` package

---
+++ packages/cli/src/actions/channel.ts
@@ -0,0 +1,75 @@
+import { PublicKey } from '@solana/web3.js'
+import log from 'loglevel'
+import { useContext } from '../context'
+
+export async function show(addr: string, _opts: any) {
+  const { client } = useContext()
+  const channel = await client.loadChannel(new PublicKey(addr))
+  log.info(JSON.stringify(channel, null, 2))
+}
+
+export async function all(_opts: any) {
+  const { client } = useContext()
+  const channels = await client.loadAllChannels()
+  log.info(`Found  ${channels.length} channels`)
+  log.info(JSON.stringify(channels, null, 2))
+}
+
+export async function deleteChannel(key: string, _opts: any) {
+  const { client } = useContext()
+
+  const channelKey = new PublicKey(key)
+
+  log.info('Deleting members...')
+  const members = await client.loadChannelMembers(channelKey)
+  log.info(`Found ${members.length} members`)
+  for (const member of members) {
+    log.info('Deleting member...')
+    const { signature } = await client.deleteMember({
+      channel: channelKey,
+      membership: member.pubkey,
+    })
+    log.info(`Sig: ${signature}`)
+  }
+
+  log.info('Deleting channel...')
+  const { signature } = await client.deleteChannel(channelKey)
+  log.info(`Sig: ${signature}`)
+  log.info('Done')
+}
+
+export async function deleteAllChannels(_opts: any) {
+  const { client } = useContext()
+
+  const channels = await client.loadAllChannels()
+  log.info(`Found  ${channels.length} channels`)
+
+  for (const channel of channels) {
+    const members = await client.loadChannelMembers(channel.pubkey)
+    log.info(`Found ${members.length} members`)
+    for (const member of members) {
+      log.info('Deleting member...')
+      const { signature } = await client.deleteMember({
+        channel: channel.pubkey,
+        membership: member.pubkey,
+      })
+      log.info(`Sig: ${signature}`)
+    }
+    log.info(`Deleting channel ${channel.pubkey}`)
+    const { signature } = await client.deleteChannel(channel.pubkey)
+    log.info(`Sig: ${signature}`)
+  }
+
+  log.info('Done')
+}
+
+export async function deleteChannelMember(channelAddr: string, membershipAddr: string, _opts: any) {
+  const { client } = useContext()
+  const { signature } = await client.deleteMember({
+    channel: new PublicKey(channelAddr),
+    membership: new PublicKey(membershipAddr),
+  })
+  log.info(`Sig: ${signature}`)
+
+  log.info('Done')
+}
