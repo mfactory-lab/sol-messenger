69f2fc96487f1e3583f1940a0f16a2385a831fc3
harniy <67821416+harniy@users.noreply.github.com>
2022-11-30 11:32:57 +0000

    Deploying to gh-pages from @ mfactory-lab/sol-messenger@252c24baff0d539bbc035f75e0270faabebe06c7 🚀

---
+++ assets/empty.810ab358.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./index.3f2eb668.js";import{Z as r,o as t,q as c}from"./vendor.c082b873.js";const n={};function s(_,a){const e=r("router-view");return t(),c(e)}const f=o(n,[["render",s]]);export{f as default};
