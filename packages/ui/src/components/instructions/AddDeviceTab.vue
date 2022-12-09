6b80aa4bf95fffa8b483284d6333c02b563b893e
harniy <forbedlam@gmail.com>
2022-12-09 13:51:21 +0200

    fix: instructions text, step

--- packages/ui/src/components/instructions/AddDeviceTab.vue
+++ packages/ui/src/components/instructions/AddDeviceTab.vue
@@ -4,6 +4,7 @@ import step2Image from '@/assets/img/instructions/add/step_2.png'
 import step3Image from '@/assets/img/instructions/add/step_3.png'
 import step4Image from '@/assets/img/instructions/add/step_4.png'
 import step5Image from '@/assets/img/instructions/add/step_5.png'
+import step6Image from '@/assets/img/instructions/add/step_6.png'
 </script>
 
 <template>
@@ -11,10 +12,13 @@ import step5Image from '@/assets/img/instructions/add/step_5.png'
     <q-list>
       <q-item v-ripple dense class="q-pa-none">
         <q-item-section>
+          <span class="add-title q-pb-none q-pt-sm">
+            For security purposes, you can only access your channels from a
+            single device. If you try to view them from another device, all
+            messages will appear encrypted.
+          </span>
           <span class="add-title q-pb-md q-pt-sm">
-            For security purposes, you can view your channels from one device
-            and if you access from another, all messages will be encrypted. To
-            add a new device, follow the instructions
+            In order to add a new device, follow these instructions:
           </span>
         </q-item-section>
       </q-item>
@@ -22,37 +26,51 @@ import step5Image from '@/assets/img/instructions/add/step_5.png'
       <q-item v-ripple dense class="q-py-md">
         <q-item-section>
           <span class="instructions-step">
-            1. Log in to the new device, press the menu and select the devices
-            field
+            1. Log in with your new device and select Devices from the menu
           </span>
           <img :src="step1Image" alt="step">
         </q-item-section>
       </q-item>
+      <q-item v-ripple dense class="q-py-md">
+        <q-item-section>
+          <span class="instructions-step">
+            2. Copy your new device key.
+          </span>
+          <img :src="step2Image" alt="step">
+        </q-item-section>
+      </q-item>
       <q-item v-ripple dense class="q-py-sm">
         <q-item-section>
-          <span class="instructions-step">2. Click on the "copy" button</span>
-          <img :src="step2Image" alt="step">
+          <span class="instructions-step">3. On your main device, select the channel to which you will add the device</span>
+          <img :src="step3Image" alt="step">
         </q-item-section>
       </q-item>
       <q-item v-ripple dense class="instructions-step">
         <q-item-section>
-          <span class="instructions-step">3. Go back to the main device and in the "devices" window click on the
-            "add device" button</span>
-          <img :src="step3Image" alt="step">
-        </q-item-section>
-      </q-item>
-
-      <q-item v-ripple dense class="q-py-sm">
-        <q-item-section>
-          <span class="instructions-step">4. Paste the previously copied new device key and click "add device"</span>
+          <span class="instructions-step">4. Pull up the Devices window and click Add Device.</span>
           <img :src="step4Image" alt="step">
         </q-item-section>
       </q-item>
 
       <q-item v-ripple dense class="q-py-sm">
         <q-item-section>
-          <span class="instructions-step">5. Confirm the transaction and your device will be added</span>
+          <span class="instructions-step">5. Enter the previously copied key and click Add.</span>
           <img :src="step5Image" alt="step">
+        </q-item-section>
+      </q-item>
+
+      <q-item v-ripple dense class="q-py-sm">
+        <q-item-section>
+          <span class="instructions-step">6. Confirm the transaction</span>
+          <img :src="step6Image" alt="step">
+        </q-item-section>
+      </q-item>
+      <q-item v-ripple dense class="q-py-sm">
+        <q-item-section>
+          <span
+            class="text-weight-medium"
+          >Congratulations, you have added a new device.
+          </span>
         </q-item-section>
       </q-item>
     </q-list>
