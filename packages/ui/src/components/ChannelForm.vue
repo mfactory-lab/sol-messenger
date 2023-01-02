cafc4a03c4bb37531824e597a5459e2d743f8bed
harniy <forbedlam@gmail.com>
2023-01-02 13:40:38 +0200

    fix: transaction expires notification

--- packages/ui/src/components/ChannelForm.vue
+++ packages/ui/src/components/ChannelForm.vue
@@ -19,42 +19,63 @@ const sendMessage = () => {
 }
 
 let timer: any
-let countDowm = 59
+let countDowm = 12
 
 const stopInterval = () => {
   clearInterval(timer)
 }
 
-watch(() => props.sending, (s) => {
-  if (s) {
-    timer = setInterval(() => {
-      if (countDowm === 0) {
-        stopInterval()
-        notify({
-          classes: 'expires-notofocation',
-          timeout: 3000,
-          message: 'Transaction expires',
-          type: 'negative',
-        })
-        countDowm = 59
-        return
-      }
-      if (countDowm === 10) {
-        notify({
-          classes: 'expires-notofocation',
-          progress: true,
-          timeout: 5000,
-          message: 'Transaction expires soon',
-          type: 'warning',
-        })
-      }
-      countDowm--
-    }, 1000)
-  } else {
-    stopInterval()
-    countDowm = 59
-  }
-})
+const showNotif = () => {
+  const notif = notify({
+    group: false,
+    timeout: 0,
+    classes: 'expires-notofocation',
+    message: 'Transaction expires',
+    type: 'warning',
+    caption: '9',
+  })
+
+  const interval = setInterval(() => {
+    notif({
+      caption: `${countDowm}`,
+    })
+
+    if (countDowm === 0 || countDowm > 10) {
+      notif({
+        timeout: 100,
+        caption: '--',
+      })
+      clearInterval(interval)
+    }
+  }, 1000)
+}
+
+watch(
+  () => props.sending,
+  (s) => {
+    if (s) {
+      timer = setInterval(() => {
+        if (countDowm === 0) {
+          stopInterval()
+          notify({
+            timeout: 3000,
+            message: 'Transaction expires',
+            type: 'negative',
+          })
+          countDowm = 12
+          return
+        }
+        if (countDowm === 10) {
+          showNotif()
+        }
+        countDowm--
+      }, 1000)
+    } else {
+      stopInterval()
+      countDowm = 12
+    }
+  },
+)
 </script>
 
 <template>
@@ -70,7 +91,9 @@ watch(() => props.sending, (s) => {
         borderless
         autofocus
         :disable="disabled"
-        :rules="[val => val.length <= 200 || 'Please use maximum 200 characters']"
+        :rules="[
+          (val) => val.length <= 200 || 'Please use maximum 200 characters',
+        ]"
       />
       <q-btn
         class="send-btn"
@@ -114,3 +137,17 @@ watch(() => props.sending, (s) => {
 }
 </style>
 
+<style lang='scss'>
+.expires-notofocation {
+  .q-notification__message {
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 10px;
+    align-items: center;
+  }
+
+  .q-notification__caption {
+    font-weight: 600;
+  }
+}
+</style>
